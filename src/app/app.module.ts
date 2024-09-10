575ba85510b9438ec5f939bcbeccb348e13eb15f
suk-6 <me@suk.kr>
2024-09-10 12:43:49 +0900

    chore: Remove unused app controller and service files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,11 @@ import * as commonModules from 'src/common/modules';
 import * as providers from 'src/common/providers';
 import * as modules from 'src/modules';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 @Module({
   imports: [
     ...importToArray(commonModules),
     ...importToArray(providers),
     ...importToArray(modules),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
