c54f7ccb7ef4a90d6af073e2c58025454467f034
Woosuk Nam <me@suk.kr>
2024-09-11 18:06:44 +0900

    feat: Add AWS SNS module for AWS integration

---
+++ src/common/modules/aws/sns/sns.service.ts
@@ -0,0 +1,25 @@
+import { SNS } from 'aws-sdk';
+
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class SnsService {
+  private sns: SNS;
+
+  constructor(private readonly configService: ConfigService) {
+    this.sns = new SNS({
+      region: this.configService.get('AWS_SNS_REGION'),
+      accessKeyId: this.configService.get('AWS_ACCESS_ID'),
+      secretAccessKey: this.configService.get('AWS_SECRET_KEY'),
+    });
+  }
+
+  sendSMS(phoneNumber: string, message: string) {
+    const params: AWS.SNS.PublishInput = {
+      Message: message,
+      PhoneNumber: phoneNumber,
+    };
+    return this.sns.publish(params).promise();
+  }
+}
