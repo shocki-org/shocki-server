7dba0b06bf64ad48e537370a0c1b6dd198c777c6
Woosuk Nam <me@suk.kr>
2024-09-11 21:44:59 +0900

    chore: Update AWS SNS service to use @aws-sdk/client-sns

--- src/common/modules/aws/sns/sns.service.ts
+++ src/common/modules/aws/sns/sns.service.ts
@@ -1,25 +1,39 @@
-import { SNS } from 'aws-sdk';
+import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
 
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SnsService {
-  private sns: SNS;
+  private sns: SNSClient;
 
-  constructor(private readonly configService: ConfigService) {
-    this.sns = new SNS({
+  constructor(
+    private readonly configService: ConfigService<
+      {
+        AWS_SNS_REGION: string;
+        AWS_ACCESS_ID: string;
+        AWS_SECRET_KEY: string;
+      },
+      true
+    >,
+  ) {
+    this.sns = new SNSClient({
       region: this.configService.get('AWS_SNS_REGION'),
-      accessKeyId: this.configService.get('AWS_ACCESS_ID'),
-      secretAccessKey: this.configService.get('AWS_SECRET_KEY'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_KEY'),
+      },
     });
   }
 
-  sendSMS(phoneNumber: string, message: string) {
-    const params: SNS.PublishInput = {
-      Message: message,
-      PhoneNumber: phoneNumber,
-    };
-    return this.sns.publish(params).promise();
+  async sendSMS(phoneNumber: string, message: string) {
+    return this.sns
+      .send(
+        new PublishCommand({
+          PhoneNumber: phoneNumber,
+          Message: message,
+        }),
+      )
+      .then((res) => res.MessageId);
   }
 }
