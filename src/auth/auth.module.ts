fe80fbfa85fc31dba524b0b973e163f7c2d75297
Woosuk Nam <me@suk.kr>
2024-09-12 10:48:39 +0900

    feat: Add PassportModule and AccessJwtStrategy to AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { PrismaModule } from 'src/common';
 
 import { AuthService } from './auth.service';
+import { AccessJwtStrategy } from './strategy/access.strategy';
 
 @Module({
-  imports: [JwtModule.register({}), PrismaModule],
-  providers: [AuthService],
+  imports: [JwtModule.register({}), PrismaModule, PassportModule],
+  providers: [AuthService, AccessJwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
