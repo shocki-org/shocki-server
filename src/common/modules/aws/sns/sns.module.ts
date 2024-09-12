e41abe3f0ba0a390f416fcee766f81518d0db782
Woosuk Nam <me@suk.kr>
2024-09-12 10:01:48 +0900

    refactor: Remove AWS SNS module and service

--- src/common/modules/aws/sns/sns.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-import { SnsService } from './sns.service';
-
-@Module({
-  providers: [SnsService],
-  exports: [SnsService],
-})
-export class SnsModule {}
