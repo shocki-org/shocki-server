0b70016567f609f135eec312ffa62b41cf066635
Woosuk Nam <me@suk.kr>
2024-09-12 10:01:54 +0900

    refactor: Update import statement for CoolsmsModule in register.module.ts

--- src/modules/register/register.module.ts
+++ src/modules/register/register.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from 'src/auth';
-import { SnsModule } from 'src/common';
+import { CoolsmsModule } from 'src/common';
 
 import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
 
 @Module({
-  imports: [SnsModule, AuthModule],
+  imports: [CoolsmsModule, AuthModule],
   controllers: [RegisterController],
   providers: [RegisterService],
 })
