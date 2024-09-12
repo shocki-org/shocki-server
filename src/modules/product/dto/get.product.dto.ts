5f62a3eecb24250ab0603bb21d65a68f2a9e8a14
Woosuk Nam <me@suk.kr>
2024-09-12 12:36:52 +0900

    feat: Add GetProductDTO for retrieving product information

---
+++ src/modules/product/dto/get.product.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+import { FundingLogDTO, ProductCategoryDTO, ProductDTO, ProductQnADTO } from './product.dto';
+
+export class GetProductDTO extends ProductDTO {
+  @ApiProperty({ description: '펀딩 로그', type: [FundingLogDTO] })
+  fundingLog: FundingLogDTO[];
+
+  @ApiProperty({ description: '상품 QnA', type: [ProductQnADTO] })
+  productQnA: ProductQnADTO[];
+
+  @ApiProperty({ description: '카테고리', type: [ProductCategoryDTO] })
+  categories: ProductCategoryDTO[];
+}
