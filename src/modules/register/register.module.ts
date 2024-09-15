67cdd412864bd9572726d53aea867c141d2f563f
suk-6 <me@suk.kr>
2024-09-16 01:53:51 +0900

    refactor: Add AlertModule to RegisterModule imports

--- src/modules/register/register.module.ts
+++ src/modules/register/register.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth';
 import { CoolsmsModule } from 'src/common';
 
+import { AlertModule } from '../alert';
 import { RegisterController } from './register.controller';
 import { RegisterService } from './register.service';
 
 @Module({
-  imports: [CoolsmsModule, AuthModule],
+  imports: [CoolsmsModule, AuthModule, AlertModule],
   controllers: [RegisterController],
   providers: [RegisterService],
 })
