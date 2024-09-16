614acd58def838a48e243a38c51a31a67df4827b
suk-6 <me@suk.kr>
2024-09-17 01:36:04 +0900

    feat: Add validation to CreateProductQnADTO

--- src/modules/product/dto/create.qna.dto.ts
+++ src/modules/product/dto/create.qna.dto.ts
@@ -1,11 +1,16 @@
 import { ProductQnA } from '@prisma/client';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProductQnADTO implements Partial<ProductQnA> {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ description: '상품 ID' })
   productId: string;
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ description: 'QnA 내용' })
   content: string;
 }
