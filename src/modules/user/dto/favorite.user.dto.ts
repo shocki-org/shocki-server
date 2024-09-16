780823e7914cdf28a77f66117fa3c6dc23e2f24b
suk-6 <me@suk.kr>
2024-09-17 01:05:30 +0900

    refactor: Remove unused FavoriteProductDTO and getFavoriteProducts method

--- src/modules/user/dto/favorite.user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UserFavorite } from '@prisma/client';
-
-import { ApiProperty } from '@nestjs/swagger';
-
-export class FavoriteProductDTO implements Partial<UserFavorite> {
-  @ApiProperty()
-  productId: string;
-}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, UseGuards } from '@nestjs/common';
+import { Controller, Delete, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import {
   ApiBadRequestResponse,
@@ -13,24 +13,12 @@ import {
 import { JwtPayload } from 'src/auth/model/payload.jwt.model';
 import { CurrentUser } from 'src/common';
 
-import { FavoriteProductDTO } from './dto/favorite.user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 @ApiTags('User')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @Get('favorite')
-  @UseGuards(AuthGuard('access'))
-  @ApiBearerAuth()
-  @ApiOperation({ summary: '좋아요 목록 불러오기' })
-  @ApiOkResponse({ description: 'Favorite products', type: [FavoriteProductDTO] })
-  @ApiNotFoundResponse({ description: 'Favorite products not found' })
-  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
-  async getFavoriteProducts(@CurrentUser() { id }: JwtPayload) {
-    return this.userService.getFavoriteProducts(id);
-  }
 
   @Delete('delete')
   @UseGuards(AuthGuard('access'))
