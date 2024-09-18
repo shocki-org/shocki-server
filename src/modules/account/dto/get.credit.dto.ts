858d8f276ffabcde0f0c9b035a526e9b2de99a3e
suk-6 <me@suk.kr>
2024-09-18 12:46:15 +0900

    refactor: Remove account module and related files

--- src/modules/account/dto/get.credit.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UserAccount } from '@prisma/client';
-
-import { ApiProperty } from '@nestjs/swagger';
-
-export class GetCreditBalanceDTO implements Partial<UserAccount> {
-  @ApiProperty({ description: '크레딧 잔액' })
-  credit: number;
-}
--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,5 +2,4 @@ export * from './register';
 export * from './product';
 export * from './user';
 export * from './alert';
-export * from './account';
 export * from './category';
