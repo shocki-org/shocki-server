cb4a133035ccfd2e53d306e2d0f1de1ac0caf526
suk-6 <me@suk.kr>
2024-09-18 14:36:06 +0900

    refactor: Update alert service to send notifications

--- src/modules/alert/alert.service.ts
+++ src/modules/alert/alert.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 
+import { FirebaseService } from 'src/common/modules/firebase/firebase.service';
 import { PrismaService } from 'src/common/modules/prisma/prisma.service';
 
 import { AlertDTO } from './dto/get.alert.dto';
 
 @Injectable()
 export class AlertService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly firebase: FirebaseService,
+  ) {}
 
   async getAlerts(userId: string): Promise<AlertDTO[]> {
     return this.prisma.alert.findMany({
@@ -25,15 +29,28 @@ export class AlertService {
   }
 
   async createAlert(alert: AlertDTO, userId: string) {
-    return this.prisma.alert.create({
-      data: {
-        ...alert,
-        user: {
-          connect: {
-            id: userId,
+    return this.prisma.alert
+      .create({
+        data: {
+          ...alert,
+          user: {
+            connect: {
+              id: userId,
+            },
           },
         },
-      },
-    });
+      })
+      .then(() => this.prisma.user.findUnique({ where: { id: userId } }))
+      .then((user) => user?.fcmToken || '')
+      .then((fcmToken) =>
+        this.firebase.sendNotificationByToken({
+          token: fcmToken,
+          title: alert.title,
+          body: alert.content,
+        }),
+      )
+      .catch(() => {
+        throw new InternalServerErrorException('알림 생성에 실패했거나, 전송에 실패했습니다.');
+      });
   }
 }
