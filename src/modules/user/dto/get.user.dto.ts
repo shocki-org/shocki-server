fb599d44c8928a079671533681cbf9b780576fb5
suk-6 <me@suk.kr>
2024-09-18 12:55:27 +0900

    feat: Add settlementAmount property to GetUserDTO

--- src/modules/user/dto/get.user.dto.ts
+++ src/modules/user/dto/get.user.dto.ts
@@ -7,4 +7,7 @@ import { UserAccountDTO, UserDTO } from './user.dto';
 export class GetUserDTO extends UserDTO {
   @ApiProperty({ description: '유저 계정', type: UserAccountDTO })
   userAccount: UserAccount;
+
+  @ApiProperty({ description: '정산 예정 금액' })
+  settlementAmount: number;
 }
