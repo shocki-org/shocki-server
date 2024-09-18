54a5995d111dae2473aa2bb09933a30036f010d1
suk-6 <me@suk.kr>
2024-09-18 12:45:19 +0900

    feat: Add ApiProperty decorator to GetCreditBalanceDTO

--- src/modules/account/dto/get.credit.dto.ts
+++ src/modules/account/dto/get.credit.dto.ts
@@ -1,5 +1,8 @@
 import { UserAccount } from '@prisma/client';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class GetCreditBalanceDTO implements Partial<UserAccount> {
+  @ApiProperty({ description: '크레딧 잔액' })
   credit: number;
 }
