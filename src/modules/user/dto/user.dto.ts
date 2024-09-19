ef2f062cf6fad66447069390ca97f917deaad7d9
suk-6 <me@suk.kr>
2024-09-19 14:42:26 +0900

    refactor: Remove settlementAmount and unused fields from UserDTO

--- src/modules/user/dto/user.dto.ts
+++ src/modules/user/dto/user.dto.ts
@@ -1,24 +1,15 @@
-import { $Enums, User } from '@prisma/client';
+import { User } from '@prisma/client';
 
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserDTO implements Partial<User> {
   @ApiProperty({ description: '유저 ID' })
   id: string;
-
-  @ApiProperty({ description: '인증 유형', enum: $Enums.Provider })
-  provider: $Enums.Provider;
-
-  @ApiProperty({ description: 'OAuth ID' })
-  providerId: string;
 }
 
 export class UserAccountDTO {
   @ApiProperty({ description: '유저 계정 ID' })
   id: string;
-
-  @ApiProperty({ description: '지갑 주소' })
-  walletAddress: string;
 
   @ApiProperty({ description: '보유 크레딧' })
   credit: number;
