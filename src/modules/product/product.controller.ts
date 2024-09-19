a48c25afae22f89f5091872c9e394f8328ec91cc
suk-6 <me@suk.kr>
2024-09-19 15:22:39 +0900

    feat: Add endpoint for purchasing market products

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -20,6 +20,7 @@ import { CurrentUser } from 'src/common';
 import { CreateProductDTO } from './dto/create.product.dto';
 import { CreateProductQnADTO } from './dto/create.qna.dto';
 import { GetProductDTO, GetProductsDTO } from './dto/get.product.dto';
+import { PurchaseMarketProductDTO } from './dto/purchase.market.dto';
 import { SearchProductDTO } from './dto/search.product.dto';
 import { UploadImageDTO } from './dto/upload.image.dto';
 import { ProductService } from './product.service';
@@ -169,7 +170,7 @@ export class ProductController {
   @Post('purchase/token')
   @ApiBearerAuth()
   @UseGuards(AuthGuard('access'))
-  @ApiOperation({ summary: '상품 구매하기' })
+  @ApiOperation({ summary: '펀딩 상품 구매하기' })
   @ApiOkResponse({ description: 'Success' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   @ApiBadRequestResponse({ description: 'Not enough amount' })
@@ -180,6 +181,27 @@ export class ProductController {
     @Query('amount') amount: number,
   ) {
     await this.productService.purchaseProductToken(id, productId, amount);
+
+    return { success: true };
+  }
+
+  @Post('purchase/market')
+  @ApiBody({
+    type: PurchaseMarketProductDTO,
+    description: 'Purchase market product',
+  })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard('access'))
+  @ApiOperation({ summary: '마켓 상품 구매하기' })
+  @ApiOkResponse({ description: 'Success' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiBadRequestResponse({ description: '잔액 부족, 또는 펀딩 상품' })
+  @ApiNotFoundResponse({ description: 'Product or User not found' })
+  async purchaseMarketProduct(
+    @CurrentUser() { id }: JwtPayload,
+    @Body() dto: PurchaseMarketProductDTO,
+  ) {
+    await this.productService.purchaseMarketProduct(id, dto);
 
     return { success: true };
   }
