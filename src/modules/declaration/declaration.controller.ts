f6d10f789b5786ea4bb09ed8b5cfdb1d520cd071
suk-6 <me@suk.kr>
2024-09-21 21:59:43 +0900

    feat: Add ApiBadRequestResponse for duplicate report in DeclarationController

--- src/modules/declaration/declaration.controller.ts
+++ src/modules/declaration/declaration.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import {
+  ApiBadRequestResponse,
   ApiBearerAuth,
   ApiBody,
   ApiOkResponse,
@@ -26,6 +27,7 @@ export class DeclarationController {
   @UseGuards(AuthGuard('access'))
   @ApiOperation({ summary: '배송 신고하기' })
   @ApiOkResponse({ description: 'Success' })
+  @ApiBadRequestResponse({ description: '이미 신고한 내역이 있습니다.' })
   @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   async report(@CurrentUser() { id }: JwtPayload, @Body() dto: ReportDTO) {
     return this.declarationService.report(id, dto);
