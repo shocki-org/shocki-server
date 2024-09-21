36b6fba306d2e1be147feb81cbc3f5b29d279524
suk-6 <me@suk.kr>
2024-09-21 22:41:59 +0900

    chore: Add BlockchainModule to UserModule imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { PrismaModule } from 'src/common';
 
+import { BlockchainModule } from '../blockchain';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, BlockchainModule],
   controllers: [UserController],
   providers: [UserService],
 })
