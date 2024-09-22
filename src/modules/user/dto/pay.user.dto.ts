e8c113a01428a2668703b5074599fb421a70c1ba
suk-6 <me@suk.kr>
2024-09-22 22:39:39 +0900

    refactor: Add API query parameters for swagger

--- src/modules/user/dto/pay.user.dto.ts
+++ src/modules/user/dto/pay.user.dto.ts
@@ -1,17 +1,28 @@
 import { IsNotEmpty, IsNumber, IsString, Length, MaxLength } from 'class-validator';
 
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PayDTO {
   @IsString()
   @MaxLength(200)
   @IsNotEmpty()
+  @ApiProperty({
+    description: '결제 키',
+  })
   paymentKey: string;
 
   @IsString()
   @Length(6, 64)
   @IsNotEmpty()
+  @ApiProperty({
+    description: '주문 번호',
+  })
   orderId: string;
 
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({
+    description: '충전 금액',
+  })
   amount: number;
 }
