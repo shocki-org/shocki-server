e2c832a88da45ebeea597c5352e2c354b87d4a3c
suk-6 <me@suk.kr>
2024-09-24 19:51:05 +0900

    fix: Add validation for image field in UploadImageDTO

--- src/modules/product/dto/upload.image.dto.ts
+++ src/modules/product/dto/upload.image.dto.ts
@@ -1,6 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UploadImageDTO {
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({ description: 'base64 이미지', type: 'string' })
   image: string;
 }
