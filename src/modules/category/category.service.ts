bf5e14f955b4f207005280a179d6a7312c799bc9
suk-6 <me@suk.kr>
2024-09-26 23:13:22 +0900

    test(sort): Sort categories with '리빙' category at the front in CategoryService

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -7,9 +7,14 @@ export class CategoryService {
   constructor(private readonly prisma: PrismaService) {}
 
   async getCategories() {
-    return this.prisma.category.findMany({
-      select: { id: true, name: true },
-    });
+    return (
+      this.prisma.category
+        .findMany({
+          select: { id: true, name: true },
+        })
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        .then((categories) => categories.sort((a, _) => (a.name === '리빙' ? -1 : 1))) // 리빙 카테고리를 가장 앞으로
+    );
   }
 
   async getProductsByCategoryId(categoryId: string) {
